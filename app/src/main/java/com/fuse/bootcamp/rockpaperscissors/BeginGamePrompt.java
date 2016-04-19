2a4f40ebad2a240b1c045afd90976e4024125c56
brad.hollander <brad.hollander@cardinalhealth.com>
2016-04-19 18:31:03 -0400

    initial project with initial prompt screen partially implemented

---
+++ app/src/main/java/com/fuse/bootcamp/rockpaperscissors/BeginGamePrompt.java
@@ -0,0 +1,26 @@
+package com.fuse.bootcamp.rockpaperscissors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class BeginGamePrompt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin_game_prompt);
+    }
+
+    public void onJoinGame(View view) {
+        EditText gameCodeEditText = (EditText) findViewById(R.id.begin_game_prompt_game_code_input);
+        String gameCode = gameCodeEditText.getText().toString();
+
+        // TODO: join game using game code
+    }
+
+    public void onCreateGame(View view) {
+        // TODO: create game and display game code
+    }
+}
