470a4f91a41ab8e5fd523b2e97d24efe764e8a12
brad.hollander <brad.hollander@cardinalhealth.com>
2016-04-20 19:15:47 -0400

    wip added onClick logic for selecting a shape

--- app/src/main/java/com/fuse/bootcamp/rockpaperscissors/ShapeSelectionActivity.java
+++ app/src/main/java/com/fuse/bootcamp/rockpaperscissors/ShapeSelectionActivity.java
@@ -2,6 +2,7 @@ package com.fuse.bootcamp.rockpaperscissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ShapeSelectionActivity extends AppCompatActivity {
 
@@ -9,5 +10,24 @@ public class ShapeSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shape_selection);
+    }
+
+    public void onShapeSelected(View view) {
+        String rockTag = getString(R.string.shape_rock);
+        String paperTag = getString(R.string.shape_paper);
+        String scissorsTag = getString(R.string.shape_scissors);
+
+        String shapeSelection = view.getTag().toString();
+        TurnResult.Shape shape = null;
+
+        if (rockTag.equals(shapeSelection)) {
+            shape = TurnResult.Shape.ROCK;
+        } else if (paperTag.equals(shapeSelection)) {
+            shape = TurnResult.Shape.PAPER;
+        } else if (scissorsTag.equals(shapeSelection)) {
+            shape = TurnResult.Shape.SCISSORS;
+        }
+
+        TurnResult result = new TurnResult(shape);
     }
 }
