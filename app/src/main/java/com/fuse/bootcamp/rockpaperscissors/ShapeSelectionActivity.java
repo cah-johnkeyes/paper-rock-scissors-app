d2dc72beb49ee73c2b2cc8f5cc1c239e9b3ed7d3
brad.hollander <brad.hollander@cardinalhealth.com>
2016-04-20 18:48:46 -0400

    username will now get saved/retrieved from shared preferences

---
+++ app/src/main/java/com/fuse/bootcamp/rockpaperscissors/ShapeSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.fuse.bootcamp.rockpaperscissors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShapeSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shape_selection);
+    }
+}
--- app/src/main/java/com/fuse/bootcamp/rockpaperscissors/UsernamePromptActivity.java
+++ app/src/main/java/com/fuse/bootcamp/rockpaperscissors/UsernamePromptActivity.java
@@ -1,13 +1,47 @@
 package com.fuse.bootcamp.rockpaperscissors;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class UsernamePromptActivity extends AppCompatActivity {
+
+    private static final String USERNAME_SHARED_PREFS_KEY = "com.fuse.bootcamp.rockpaperscissors.USERNAME_SHARED_PREFS_KEY";
+
+    private SharedPreferences sharedPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_username_prompt);
+
+        sharedPrefs = getPreferences(MODE_PRIVATE);
+        String username = sharedPrefs.getString(USERNAME_SHARED_PREFS_KEY, "");
+
+        if (!username.isEmpty()) {
+            promptToStartOrJoinGame();
+        } else {
+            setContentView(R.layout.activity_username_prompt);
+        }
+    }
+
+    private void promptToStartOrJoinGame() {
+        Intent intent = new Intent(this, BeginGamePromptActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSubmitUsername(View view) {
+        EditText usernameEditText = (EditText) findViewById(R.id.username_prompt_edit_text);
+        String username = usernameEditText.getText().toString();
+
+        if (!username.isEmpty()) {
+            SharedPreferences.Editor editor = sharedPrefs.edit();
+            editor.putString(USERNAME_SHARED_PREFS_KEY, username);
+            editor.apply();
+
+            promptToStartOrJoinGame();
+        }
     }
 }
