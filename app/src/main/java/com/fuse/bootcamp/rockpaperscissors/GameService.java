6f1228ca628b1ceadbb3841437150bfaa9ac3902
brad.hollander <brad.hollander@cardinalhealth.com>
2016-04-21 18:29:29 -0400

    added more calls to the service interface

--- app/src/main/java/com/fuse/bootcamp/rockpaperscissors/GameService.java
+++ app/src/main/java/com/fuse/bootcamp/rockpaperscissors/GameService.java
@@ -3,9 +3,16 @@ package com.fuse.bootcamp.rockpaperscissors;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface GameService {
 
     @POST("/player")
     Call<Player> submitPlayer(@Body Player player);
+
+    @POST("/game/{gameId}/player/{username}")
+    Call<Void> joinGame(@Path("gameId") String gameId, @Path("username") String username);
+
+    @POST("/game/{gameId}/player/{username}/message")
+    Call<Void> sendMessage(@Path("gameId") String gameId, @Path("username") String username, @Body String message);
 }
